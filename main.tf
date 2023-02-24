2e2d5f047ccb34027c47ae164b0b399028a2e0fc
Guangyu Wang <wang.guangyu1@northeastern.edu>
2023-02-23 23:24:45 -0500

    add instance

--- main.tf
+++ main.tf
@@ -61,3 +61,64 @@ resource "aws_route_table_association" "private_subnet_asso" {
  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
  route_table_id = aws_route_table.second_rt_private.id
 }
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-"
+  description = "Example security group"
+  vpc_id =aws_vpc.Guangyu.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_instance" "example"{
+  ami=var.ami_id//"ami-0c6c41abbb70d840e"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_subnets[0].id
+  key_name = var.key_pair
+  instance_initiated_shutdown_behavior = "stop"
+  disable_api_termination = true
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.example.id]
+  root_block_device {
+    #device_name = "/dev/xvda"
+    volume_size = 50
+    volume_type = "gp2"
+
+    delete_on_termination = true
+    # other root device settings
+  }
+}
